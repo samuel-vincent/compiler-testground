a48637b1406ec375546e80ef2b65ab28f2a8af11
Samuel Vincent Paal <samuel.v.paal@gmail.com>
2011-08-31 12:18:21 +0300

    Support x86 and x86-64 instruction sets.

--- Makefile
+++ Makefile
@@ -3,14 +3,24 @@ SRC=$(wildcard *.asm)
 EXEC=$(SRC:%.asm=%)
 OBJ=$(SRC:%.asm=%.o)
 
+LBITS=$(shell getconf LONG_BIT)
+
+ifeq ($(LBITS),64)
+	ASM_FMT=elf64
+	ARCH="x86-64"
+else
+	ASM_FMT=elf
+	ARCH="x86-32"
+endif
+
 compiler: compiler.hs
 	ghc --make compiler
-	./compiler "./test.asm" ""
+	./compiler "./test.asm" "" $(ARCH)
 
 compile: $(EXEC)
 
 %.o: %.asm
-	nasm -f elf -g -F dwarf $<
+	nasm -f $(ASM_FMT) -g -F dwarf $<
 
 %: %.o
 	gcc $< -o $@
--- compiler.hs
+++ compiler.hs
@@ -3,52 +3,82 @@ import System(getArgs)
 
 main :: IO ()
 main = do
-  (f:src:[]) <- getArgs
-  writeFile f (compile src)
+  (f:src:platform:[]) <- getArgs
+  writeFile f (compile src (if platform == "x86-32" then X86_32 else X86_64))
+
+data Architecture = X86_32 | X86_64
+
+ax :: Architecture -> String
+ax (X86_32) = "eax"
+ax (X86_64) = "rax"
+
+bx :: Architecture -> String
+bx (X86_32) = "ebx"
+bx (X86_64) = "rbx"
 
 insn :: String -> String
 insn x = "\n\t" ++ x
 
-binop :: Expr -> Expr -> String -> String
-binop l r cmd = gen l ++ gen r ++
-                insn "pop eax" ++
-                insn "pop ebx" ++
+binop :: Architecture -> Expr -> Expr -> String -> String
+binop a l r cmd = gen a l ++ gen a r ++
+                insn ("pop " ++ (ax a)) ++
+                insn ("pop " ++ (bx a)) ++
                 insn cmd ++
-                insn "push eax"
+                insn ("push " ++ (ax a))
 
-gen :: Expr -> String
-gen (Addop l r) = binop l r "add eax, ebx"
-gen (Subop l r) = binop l r ("sub eax, ebx" ++ insn "neg eax")
-gen (Mulop l r) = binop l r "mul ebx"
-gen (Const x) = insn "mov eax, " ++ (show x) ++
-                insn "push eax"
+gen :: Architecture -> Expr -> String
+gen a (Addop l r) = binop a l r ("add " ++ (ax a) ++ ", " ++ (bx a))
+gen a (Subop l r) = binop a l r ("sub " ++ (ax a) ++ ", " ++ (bx a) ++ 
+                               insn "neg " ++ (ax a))
+gen a (Mulop l r) = binop a l r ("mul " ++ (bx a))
+gen a (Const x) = insn "mov " ++ (ax a) ++ ", " ++ (show x) ++
+                  insn ("push " ++ (ax a))
 
-header :: String
-header = "extern printf\n" ++ 
-         "segment .data\n\t" ++ 
-         "msg db \"Result: %i\", 0xA\n" ++
-         "segment .bss\n" ++ 
-         "segment .text\n\t" ++
-         "global main\n" ++
-         "main:\n\t" ++
-         "push ebp\n\t" ++
-         "mov ebp, esp\n"
-         
-footer :: String
-footer = "\n\t" ++
-         "push dword msg\n\t" ++
-         "call printf\n\t" ++
-         "add esp, 8\n\t" ++
-         "mov esp, ebp\n\t" ++
-         "pop ebp\n\t" ++
-         "mov eax, 0\n\t" ++
-         "ret"
+header :: Architecture -> String
+header (X86_32) = "extern printf\n" ++ 
+                  "segment .data\n\t" ++ 
+                  "msg db \"Result: %i\", 0xA\n" ++
+                  "segment .bss\n" ++ 
+                  "segment .text\n\t" ++
+                  "global main\n" ++
+                  "main:\n\t" ++
+                  "push ebp\n\t" ++
+                  "mov ebp, esp\n"
+                  
+header (X86_64) = "extern printf\n" ++
+                  "segment .data\n\t" ++
+                  "msg db \"Result: %i\", 0xA\n" ++
+                  "segment .bss\n" ++
+                  "segment .text\n\t" ++
+                  "global main\n" ++
+                  "main:\n\t" ++
+                  "push rbp\n\t" ++
+                  "mov rbp, rsp\n"
 
-compile :: String -> String
-compile _ = header ++ 
-            gen (Subop (Const 100) 
-                 (Addop (Mulop (Const 20) (Const 2)) (Const 50))) ++ 
-            footer
+footer :: Architecture -> String
+footer (X86_32) = "\n\t" ++
+                  "push dword msg\n\t" ++
+                  "call printf\n\t" ++
+                  "add esp, 8\n\t" ++
+                  "mov esp, ebp\n\t" ++
+                  "pop ebp\n\t" ++
+                  "mov eax, 0\n\t" ++
+                  "ret"
+                  
+footer (X86_64) = "\n\t" ++
+                  "pop rsi\n\t" ++
+                  "mov rdi, msg\n\t" ++
+                  "call printf\n\t" ++
+                  "add rsp, 8\n\t" ++
+                  "mov rax, 0\n\t" ++
+                  "leave\n\t" ++
+                  "ret"
+
+compile :: String -> Architecture -> String
+compile _ a = header a ++ 
+              gen a (Subop (Const 100) 
+                   (Addop (Mulop (Const 20) (Const 2)) (Const 50))) ++ 
+              footer a
 
 type Term = Int
 
